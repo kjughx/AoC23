910f1fa0c988d09ab40260c18df6dbbc2294f720
Philip J. <kjughx@gmail.com>
2023-12-05 17:48:55 +0100

    Day5 part2 done

--- src/day5.py
+++ src/day5.py
@@ -5,14 +5,14 @@ maps = [{} for _ in range(7)]
 lines = [line.strip('\n') for line in open("../inputs/day5").readlines()]
 seeds = list(map(int, lines[0].split(": ")[1].split(" ")))
 
-id=0
+id = 0
 for line in lines[2::]:
     if len(line) == 0:
         id += 1
         continue
     elif line[0].isalpha():
         continue
-    dst,src,r = map(int, line.split(" "))
+    dst, src, r = map(int, line.split(" "))
     maps[id][src] = (dst, r)
 
 locations = []
@@ -30,30 +30,32 @@ for seed in seeds:
     locations.append(seed)
 print(min(locations))
 
-seeds = [(seeds[i], seeds[i] + seeds[i+1]) for i in range(len(seeds)-1)]
-# split ranges into posssibly three ranges
-mins = []
+seeds = set([(seeds[2*i], seeds[2*i] + seeds[2*i+1] - 1) for i in range(int(len(seeds)/2))])
+
+# split the ranges in seeds into posssibly three ranges
 for m in maps:
-    mseeds = []
+    mseeds = set()  # seeds that were mapped by a range
     for (src, (dst, r)) in m.items():
-        nseeds = []
+        nseeds = set()  # seeds that were not mapped, these need to be checked again
         while (len(seeds) > 0):
             ss, se = seeds.pop()
+
             # how to find seeds in [src, src + r]?
             # the seed range in the source range: [srs, sre)
             srs = max(ss, src)
             sre = min(src + r, se)
 
-            if ss < src: # before source
-                nseeds.append((ss, src))
-            if se >= src + r: # after source
-                nseeds.append((src+r, se))
+            if ss < src:  # before source
+                nseeds.add((ss, min(src-1, se)))  # All seeds that are before src
 
-            if srs < sre: # then there are seeds in the source range
-                mseeds.append((dst + (srs - src), dst + (sre - src)))
-        seeds = nseeds
-    seeds = seeds + mseeds
-    mins.append(min(seeds)[0])
-    print(mins)
-print(min(mins))
+            if se >= src + r:  # after source
+                nseeds.add((max(src + r, ss), se))  # All seeds that are after (and including) src + r
 
+            if srs <= sre:  # then there are seeds in the source range
+                mseeds.add((dst + (srs - src), dst + (sre - src)))
+
+        seeds = nseeds  # Check the seeds that were not already mapped again
+
+    seeds |= mseeds  # Add the mapped seeds back to seeds
+
+print(min(seeds)[0])
