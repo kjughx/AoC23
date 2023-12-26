b633f1b827c0dedd93682bc2a36cd1ca89bfff59
Philip J. <kjughx@gmail.com>
2023-12-26 20:25:47 +0100

    Day 14, done

--- src/day14.py
+++ src/day14.py
@@ -1,34 +1,55 @@
-#!/usr/bin/env python3
-# part 1 done somewhere else
+#!/bin/env python3
+import numpy as np
 from collections import deque
 
-import numpy as np
+grid = np.array([[c for c in line.strip()] for line in open("../inputs/day14").readlines()])
 
-lines = np.array([[c for c in line.strip()] for line in open('../inputs/day14').readlines()])
-R = lines.shape[0]
-C = lines.shape[1]
+R = grid.shape[0]
+C = grid.shape[1]
 
-os = list(zip(np.where(lines == 'O')[0], np.where(lines == 'O')[1]))
-hs = list(zip(np.where(lines == '#')[0], np.where(lines == '#')[1]))
+def shift(grid, d):
+    # split on '#' and shift the O's to the "beginning"
+    for i in range(R):
+        if d in 'ns':
+            slz = grid[:, i]
+        else:
+            slz = grid[i, :]
 
-def roll(dr, dc, new):
-    roll = True
-    while roll:
-        old = new
-        new = []
-        roll = False
-        for r,c in old:
-            nr,nc = r + dr, c + dc
-            if lines[nr, nc] == '.' and 0 <= nr < R and 0 <= nc < C:
-                new.append((nr, nc))
-                roll = True
-        for r,c in new:
-            lines[nr, nc] = 'O'
-            lines[r, c] = '.'
+        os = [sorted(o, reverse=(d in "nw")) for o in ''.join(slz).split('#')]
+        slz = [c for c in "#".join([''.join(o) for o in os])]
 
-    return new
+        if d in 'ns':
+            grid[:, i] = slz
+        else:
+            grid[i, :] = slz
 
+    return grid
 
-new = roll(-1, 0, os)
-print(lines)
+grid = shift(grid, 'n')
+os = list(zip(np.where(grid == 'O')[0], np.where(grid == 'O')[1]))
+t = 0
+for r, _ in os:
+    t += (R-r)
+print(t)
 
+seen = []
+N = 0
+while True:
+    os = list(zip(np.where(grid == 'O')[0], np.where(grid == 'O')[1]))
+    if os in seen:
+        break
+    N += 1
+
+    seen.append(os)
+    for d in ['n', 'w', 's', 'e']:
+        grid = shift(grid, d)
+
+os = list(zip(np.where(grid == 'O')[0], np.where(grid == 'O')[1]))
+# So now we know it repeats, the final 'os' is then:
+i = seen.index(os)
+os = seen[i + (1000000000 - i) % (N - i)]
+
+t = 0
+for r, _ in os:
+    t += (R-r)
+print(t)
