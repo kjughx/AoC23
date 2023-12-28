616394a6b2efba0c25a27b4f773ab6fb86675250
Philip J. <kjughx@gmail.com>
2023-12-28 15:49:38 +0100

    Day 18, done...

--- src/day18.py
+++ src/day18.py
@@ -2,8 +2,9 @@
 import numpy as np
 
 lines = [line.strip() for line in open("../inputs/day18").readlines()]
-
+r,c = 0,0
 loop = []
+length = 0
 for line in lines:
     d, l, color = line.split(' ')
 
@@ -17,9 +18,33 @@ for line in lines:
     elif d == 'U':
         r, c = r - l ,c
 
+    length += l
     loop.append((r, c, color))
 
-rs = set()
-for i, (r,c, _) in enumerate(loop):
+A = abs(sum(loop[i][0] * (loop[i - 1][1] - loop[(i + 1) % len(loop)][1]) for i in range(len(loop)))) // 2
+print(length + (A - length// 2 + 1))
 
-print(t + 1)
+
+loop = []
+length = 0
+for line in lines:
+    _, _, color = line.split(' ')
+    color = int(color[2:-1], 16)
+    d = color & 0xf
+    l = color & 0xfffff0
+    l >>= 4
+
+    if d == 0:
+        r,c = r, c + l
+    elif d == 1:
+        r,c = r + l, c
+    elif d == 2:
+        r,c = r, c - l
+    elif d == 3:
+        r, c = r - l ,c
+
+    length += l
+    loop.append((r, c, color))
+
+A = abs(sum(loop[i][0] * (loop[i - 1][1] - loop[(i + 1) % len(loop)][1]) for i in range(len(loop)))) // 2
+print(length + (A - length// 2 + 1))
